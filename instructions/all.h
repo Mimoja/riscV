8a224e11a7b027a7df0025bb4c1a7044b7094c02
Mimoja <max-schander@freenet.de>
2017-06-22 01:56:24 +0200

    cleanup instruction header import

---
+++ instructions/all.h
@@ -0,0 +1,47 @@
+//
+// Created by mimoja on 22.06.17.
+//
+
+#ifndef RISCV_ALL_H
+#define RISCV_ALL_H
+
+#include "Instruction.h"
+
+#include "ADD.h"
+#include "ADDI.h"
+#include "AND.h"
+#include "ANDI.h"
+#include "AUIPC.h"
+#include "BEQ.h"
+#include "BGE.h"
+#include "BGEU.h"
+#include "BLT.h"
+#include "BLTU.h"
+#include "BNE.h"
+#include "JAL.h"
+#include "JALR.h"
+#include "LB.h"
+#include "LBU.h"
+#include "LH.h"
+#include "LHU.h"
+#include "LUI.h"
+#include "LW.h"
+#include "OR.h"
+#include "ORI.h"
+#include "SB.h"
+#include "SH.h"
+#include "SLL.h"
+#include "SLLI.h"
+#include "SLT.h"
+#include "SLTI.h"
+#include "SLTIU.h"
+#include "SLTU.h"
+#include "SRA.h"
+#include "SRAI.h"
+#include "SRL.h"
+#include "SRLI.h"
+#include "SUB.h"
+#include "SW.h"
+#include "XOR.h"
+#include "XORI.h"
+#endif //RISCV_ALL_H
