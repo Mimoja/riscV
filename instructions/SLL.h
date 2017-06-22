3b57aa2a55bf5357b39964a26982899e00351056
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2017-06-22 03:10:40 +0200

    Fix five-bit mask in shift operations

--- instructions/SLL.h
+++ instructions/SLL.h
@@ -15,7 +15,7 @@ namespace instructions {
             sprintf(disas_buffer, "SLL %d, %d, %d", instr.R.rd, instr.R.rs1, instr.R.rs2);
         }
         void execute(registers* reg, memory* mem) {
-            reg->setReg32(instr.I.rd , reg->getReg32(instr.R.rs1) << (reg->getReg32(instr.R.rs2) & 0xFFFFF));
+            reg->setReg32(instr.I.rd, reg->getReg32(instr.R.rs1) << (reg->getReg32(instr.R.rs2) & 0x1F));
         }
     };
 
--- instructions/SRA.h
+++ instructions/SRA.h
@@ -11,12 +11,12 @@ namespace instructions {
     class SRA : public Instruction {
     public:
         SRA(const decode::instruction_type &decoded) : Instruction(decoded) {
-            sprintf(disas_buffer, "SRA %d, %d, %d", instr.R.rd, instr.R.rs1, instr.R.rs2 & 0xFFFFF);
+            sprintf(disas_buffer, "SRA %d, %d, %d", instr.R.rd, instr.R.rs1, instr.R.rs2 & 0x1F);
         }
         void execute(registers* reg, memory* mem) {
             uint32_t regVal = reg->getReg32(instr.R.rs1);
-            uint32_t value =  regVal >> (reg->getReg32(instr.R.rs2) & 0xFFFFF);
-            if(regVal & BIT(31))value |= 0xFFFFFFFF << (32-(reg->getReg32(instr.R.rs2) & 0xFFFFF));
+            uint32_t value =  regVal >> (reg->getReg32(instr.R.rs2) & 0x1F);
+            if(regVal & BIT(31))value |= 0xFFFFFFFF << (32-(reg->getReg32(instr.R.rs2) & 0x1F));
             reg->setReg32(instr.I.rd, value);
         }
     };
