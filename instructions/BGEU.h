af30a3b359e40106e2e5b56e1479e1e3adf1841d
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2017-06-22 03:10:40 +0200

    Fix BGEU operation

--- instructions/BGEU.h
+++ instructions/BGEU.h
@@ -14,7 +14,7 @@ namespace instructions {
             sprintf(disas_buffer, "BGEU %d, %d %08X", instr.B.rs1, instr.B.rs2, instr.B.getImm());
         }
         void execute(registers* reg, memory* mem) {
-            if(reg->getReg32(instr.B.rs1) < reg->getReg32(instr.B.rs2))
+            if(reg->getReg32(instr.B.rs1) >= reg->getReg32(instr.B.rs2))
                 reg->setPC32(reg->getPC32()+instr.B.getImm());
         }
     };
