e2dd4ae828212fe15b9ae0ebf76ed63040f0a715
Mimoja <max-schander@freenet.de>
2017-06-24 05:17:11 +0200

    Copy from elf sections instead of loading file

--- main.cpp
+++ main.cpp
@@ -20,12 +20,14 @@ uint32_t bytecode[] = {
         0x00008067,          // ret
 };
 
+
 int main(int argc, char** argv) {
 
     if ( argc == 1 || argc > 2) {
         printf("Usage: %s <elf_file>\n", argv[0]);
         return 1;
     }
+
     ELFIO::elfio reader;
 
     if ( !reader.load( argv[1] ) ) {
@@ -40,33 +42,35 @@ int main(int argc, char** argv) {
         printf("Wrong ELF encoding: Not Little Endian\n");
         return 4;
     }
-
-    std::ifstream file(argv[1], std::ios::binary | std::ios::ate);
-    std::streamsize size = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    std::vector<char> elf_buffer(size);
-    if (!file.read(elf_buffer.data(), size))
-    {
-        printf("File read failed");
+    if( reader.get_machine() != EM_RISCV){
+        printf("Wrong machine in ELF!\n");
         return 5;
     }
-    printf("ELF Size is %d bytes\n", elf_buffer.size());
 
-    memory* mem = new memory(500000+size+0x00010000);
+    ELFIO::Elf_Half seg_num = reader.segments.size();
+    memory* mem = new memory(50*1024*1024);
+    for ( int i = 0; i < seg_num; ++i ) {
+        const ELFIO::segment* pseg = reader.segments[i];
+
+        if(pseg->get_type() == PT_LOAD){
+            printf("Copying %d bytes from segment %d to 0x%08X\n", pseg->get_memory_size(),i , pseg->get_physical_address());
+            mem->copyToMem((uint8_t*)  reader.segments[i]->get_data(), pseg->get_physical_address(), pseg->get_memory_size());
+        }
+    }
+
     registers* reg = new registers();
 
-    mem->copyToMem((uint8_t*) &elf_buffer[0], 0x00010000, elf_buffer.size());
-    reg->setPC32(reader.get_entry());
+    reg->setPC32(reader.get_entry()-4);
     reg->setReg32(2,mem->getSize());
 
     printf("Entry addr is 0x%08X, offset: %d bytes\n", reader.get_entry(),reader.get_entry());
     instructions::Instruction* instruction = nullptr;
 
-    while(true){
+    do{
+        reg->setPC32(reg->getPC32()+4);
         try {
             instruction = decode::decode_instruction(mem->getWord(reg->getPC32()));
-            printf("0x%08X: %s\n",reg->getPC32(),instruction->to_string());
+            printf("0x%08X: 0x%08X: %s\n",reg->getPC32(),mem->getWord(reg->getPC32()),instruction->to_string());
             instruction->execute(reg, mem);
         }catch( const std::out_of_range& e ) {
             printf("Expception: %s\n", e.what());
@@ -79,7 +83,7 @@ int main(int argc, char** argv) {
             printf("%s \n", c);
             return 7;
         }
-        reg->setPC32(reg->getPC32()+4);
-    }
+    }while(reg->getPC32() != 0);
+    printf("Return is %u\n", reg->getReg32(10));
     return 0;
 }
