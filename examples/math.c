68246ae3c35f56549dea3a496519bb75d5e70d4d
Mimoja <max-schander@freenet.de>
2017-06-24 05:17:28 +0200

    Update examples

--- examples/math.c
+++ examples/math.c
@@ -1,4 +1,10 @@
-//
-// Created by mimoja on 24.06.17.
-//
 
+
+int _start(){
+    volatile int a = 2;
+    volatile int b = 5;
+    volatile int c = a + b;
+    volatile int d = a * c;
+    volatile int e = d / 4;
+    return e;
+}
